7ed8367239bf43b3923e9e9a6b87a965712a6f79
JovianRios <36941795+JovianRios@users.noreply.github.com>
2021-01-28 16:11:39 -0500

    Update person.js

--- person.js
+++ person.js
@@ -4,16 +4,11 @@ class Person {
     constructor(name, favoriteColor) {
         this.name = name;
         this.favoriteColor = favoriteColor;
-
     }
-
     speak() {
-
         for(let i = 0; i < this.favoriteColor.length; i++) {
             console.log(chalk.hex(this.favoriteColor[i])(this.name));
         }
     }
-
 }
-
 module.exports = Person;
