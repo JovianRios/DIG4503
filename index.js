7d66e4954c8c963cd31b740e692ee8979cc45d86
JovianRios <36941795+JovianRios@users.noreply.github.com>
2021-03-26 11:52:45 -0400

    Second Commit for Lab 9

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ let fileContents = fs.readFileSync("database.json");
 
 let database = JSON.parse(fileContents);
 
+App.use("/", Express.static("public"));
+
 App.get("/employees/:name",(req, res) => {
-    let result = {"error": "not found"};
+    let result = {"error": "Name not found!"};
 
     database.forEach((value) => {
         if(req.params.name == value.name) {
@@ -21,7 +23,7 @@ res.json(result);
 
 
 App.get("/ages/:number", (req, res) => {
-    let result = {"error": "not found"};
+    let result = {"error": "Age not found!"};
 
     database.forEach((value) => {
         if(req.params.number == value.age) {
@@ -30,6 +32,19 @@ App.get("/ages/:number", (req, res) => {
 });
 res.json(result);
 })
+
+App.post("/employees/:name/:age", (req, res) => {
+    let result = {
+        "name": req.params.name,
+        "age": parseInt(req.params.age)
+    };
+
+    database.push(result);
+
+    fs.writeFileSync("database.json", JSON.stringify(database, null, '\t'));
+
+    res.json(result);
+});
 
 App.listen(port, () => {
     console.log("Server running!");
